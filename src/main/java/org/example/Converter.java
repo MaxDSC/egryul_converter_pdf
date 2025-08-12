061345c3a41a1daa2113e0f84eb4cf804c4378c7
Programmer <spir1n.maks@yandex.ru>
2025-08-12 21:35:47 +0300

    Добавлены методы парсинга 3-ого столбца выписки - private void setTemplates(); - public void findJsonKeys(); - private boolean containsRegExp(String[] arrTemplate); - public String[] setRegexpInMarkedString(String[] arrTemplate). Парсинг 3-ого столбца осуществляется при помощи файла templates.csv, в котором записаны: шаблон (соответствует 2-ому столбцу выписки); ключ JSON для этого шаблона; до 9 штук регулярных выражений, которые последовательно могут быть подставлены в шаблон через %1-%9. Добавлены javadoc к методам класса Converter.java
    Signed-off-by: Programmer <spir1n.maks@yandex.ru>

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -7,11 +7,12 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Objects;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,29 +22,62 @@ public class Converter {
     private static final float A4_WIDTH = 210;
     private final float WIDTH_FIRST_COL;
     private final float WIDTH_TWO_COL;
-    ArrayList<String> text;
-    ArrayDeque<String> rows;
+    private final String SPLIT_TEMPLATE = ";";
+    private final String MARK_REG_EXP = "%";
+    private ArrayList<String> text;
+    private ArrayDeque<String> rows;
+    private ArrayList<String> templates;
+    private Map<String, String> jsonKeys;
 
 
-    public Converter(String pathName) throws IOException {
+    /**
+     * Конструктор конвертера. Сразу извлекает шаблоны строк из файла templates.csv.
+     *
+     * @param pathName - путь к файлу PDF-выписки.
+     * @throws IOException        -неудачная операция с файлом выписки.
+     * @throws URISyntaxException - неудачная операция с файлом шаблонов.
+     */
+    //TODO: Добавить проверку, что путь ведет именно к PDF-файлу
+    public Converter(String pathName) throws IOException, URISyntaxException {
         doc = Loader.loadPDF(new File(pathName));
         countPage = doc.getNumberOfPages();
         WIDTH_FIRST_COL = 31;
         WIDTH_TWO_COL = 113.4f;
         rows = new ArrayDeque<String>();
+        jsonKeys = new HashMap<>();
+        setTemplates();
     }
 
+    /**
+     * Получить весь текст PDF-выписки с конкретной страницы.
+     *
+     * @param number - номер страницы
+     * @return строку, в которой будет весь текст PDF-выписки.
+     * @throws IllegalArgumentException - номер страницы больше максимальной или отрицательный.
+     */
     public PDPage getPage(int number) {
         if (number > countPage || number < 0) throw new IllegalArgumentException("A non-existent page is specified");
         return doc.getPage(number);
     }
 
+    /**
+     * Получить весь текст PDF-выписки.
+     *
+     * @return строку, в которой будет весь текст PDF-выписки.
+     */
     public String getText() throws IOException {
         PDFTextStripper pdfStripper = new PDFTextStripper();
         return pdfStripper.getText(doc);
     }
 
-    public void splitTextToRowsTable(int startNumberPage, int endNumberPage) throws IOException {
+    /**
+     * Создает очередь строк, которые представляют собой строку таблицы выписки PDF из диапазона страниц.
+     *
+     * @param startNumberPage страница, с которой начинается анализ.
+     * @param endNumberPage   страница, на которой нужно остановить анализ.
+     * @return очередь @{@link ArrayDeque}, в которой будут строки таблицы выписки.
+     */
+    public ArrayDeque<String> splitTextToRowsTable(int startNumberPage, int endNumberPage) throws IOException {
         if (startNumberPage < 0 || startNumberPage > countPage || endNumberPage < 0 || endNumberPage > countPage || endNumberPage < startNumberPage)
             throw new IllegalArgumentException("A non-existent page is specified");
         for (int i = startNumberPage; i <= endNumberPage; i++) {
@@ -53,27 +87,127 @@ public class Converter {
             String text = pdfStripper.getText(doc);
             extractTable(text, rows);
         }
-        for (String s : rows) System.out.println(s);
+        return rows;
     }
 
-    public void splitTextToRowsTable() throws IOException {
-       splitTextToRowsTable(1, 17);
+    /**
+     * Создает очередь строк, которые представляют собой строку таблицы выписки PDF из всех страниц.
+     *
+     * @return очередь @{@link ArrayDeque}, в которой будут строки таблицы выписки из всех страниц.
+     */
+    public ArrayDeque<String> splitTextToRowsTable() throws IOException {
+        return splitTextToRowsTable(1, countPage);
     }
 
-    //Превратит исходный текст, который заключен в таблице PDF в нумерованные строки таблицы очереди.
-    private void extractTable(String text, ArrayDeque<String> list) {
+    /**
+     * Выведет значение 3 столбца выписки (Значение показателя), согласно загруженным шаблонам.
+     * Ищет в коллекции строк таблицы выписки строки по шаблонам из коллекции шаблонов. Шаблоны соответствуют значениям из второго столбца выписки.
+     * Если строка из таблицы имеет подстроку из шаблона (или по регулярному выражению), то из строки таблицы вырезается шаблон, таким образом, оставляя только значение 3-ого столбца таблицы выписки.
+     * При этом удаляется элемент шаблона из коллекции шаблонов, для анализа тех шаблонов, которые не удалось найти.
+     */
+    public void findJsonKeys() {
+        if (rows == null || rows.isEmpty())
+            throw new IllegalArgumentException("There is no data to search!");
+        if (templates == null || templates.isEmpty())
+            throw new IllegalArgumentException("Templates is null or empty!");
+        for (String r : rows) {
+            String findedT = "";
+            for (String t : templates) {
+                String[] arrT = t.split(SPLIT_TEMPLATE);
+                boolean isRegex = containsRegExp(arrT);
+                String[] twoExp = setRegexpInMarkedString(arrT);
+                Pattern pattern = Pattern.compile(twoExp[0]);
+                Matcher matcher = pattern.matcher(r);
+                if (matcher.find()) {
+                    int endSub = matcher.end();
+                    if (isRegex) {
+                        Pattern innerPatter = Pattern.compile(twoExp[1]);
+                        Matcher innerMatcher = innerPatter.matcher(r);
+                        if (innerMatcher.find()) endSub = innerMatcher.end();
+
+                    }
+                    String value = r.substring(endSub);
+                    System.out.println("R---> " + r);
+                    System.out.println("S---> " + twoExp[0]);
+                    System.out.println("V--->" + value + "\n");
+                    findedT = t;
+                    break;
+                }
+            }
+            templates.remove(findedT);
+        }
+        if (templates.isEmpty()) {
+            System.out.println("All templates is found!");
+        } else {
+            System.out.println("Not founded templates:");
+            for (String s : templates) System.out.println(s);
+        }
+    }
+
+    /**
+     * Содержит ли массив строк, собранный из строки шаблона регулярное выражение. Строка шаблона не имеет регулярного выражения, если содержит только два члена, идущих через точку с запятой.
+     *
+     * @param arrTemplate массив строк, полученный через split строки-шаблона.
+     * @return true - если имеет регулярное выражение, false - если не имеет регулярного выражения.
+     */
+    private boolean containsRegExp(String[] arrTemplate) {
+        return arrTemplate.length > 2;
+    }
+
+    /**
+     * Подставить регулярное выражение в строку из arrTemplate[0], которая имеет формат "Строка%1", где %1 - это место подставки регулярного выражения.
+     * Таких подстановок может быть до 9 штук (%1-%9), регулярные выражения соответствующие подстановкам начинаются со 2-ого индекса.
+     * Метод подставит в %1 - arrTemplate[2], в %9 - arrTemplate[10], но если маркера подстановки нет (только 2 элемента массива), то вернет исходный arrTemplate[0].
+     *
+     * @param arrTemplate массив строк, полученный через split строки-шаблона.
+     * @return массив из двух элементов, где 0 - строка с включенным в нее регулярным выражение, подставленным в %1-%9 arrTemplate[0], в 1 - это значение arrTemplate[0] без подстановки регулярного выражения (без %1-%9).
+     * Если строка шаблона не имеет регулярного выражения, то элементы в массив одинаковы и равны строке шаблона.
+     */
+    public String[] setRegexpInMarkedString(String[] arrTemplate) {
+        String sub = arrTemplate[0];
+        String[] result = {sub, sub};
+        if (containsRegExp(arrTemplate)) {
+            StringBuilder sb = new StringBuilder(sub);
+            StringBuilder strWithoutRegexp = new StringBuilder(sub);
+            for (int i = 2; i < arrTemplate.length; i++) {
+                Pattern pattern = Pattern.compile(MARK_REG_EXP + (i - 1));
+                Matcher matcher = pattern.matcher(sb);
+                if (matcher.find()) {
+                    sb.replace(matcher.start(), matcher.end(), arrTemplate[i]);
+                    int indexMARK_REG_EXP = strWithoutRegexp.indexOf(MARK_REG_EXP + (i - 1));
+                    strWithoutRegexp.replace(indexMARK_REG_EXP, indexMARK_REG_EXP + 2, "");
+                }
+            }
+            result[0] = sb.toString();
+            result[1] = strWithoutRegexp.toString();
+        }
+        return result;
+    }
+
+    /**
+     * Превратит исходный текст, который заключен в таблице выписки в очередь строк таблицы.
+     *
+     * @param text весь текст из PDF-выписки.
+     * @param rows очередь @{@link ArrayDeque}, в которую загружаются строки таблицы.
+     */
+    private void extractTable(String text, ArrayDeque<String> rows) {
         String[] arrText = text.split("\r\n");
         int endTable = getLowestBorerTable(arrText);
-        identifyNumberedLines(list, arrText);
-        concatAssociatedStrings(list, arrText, endTable);
-        fixNumbering(list);
+        identifyNumberedLines(rows, arrText);
+        concatAssociatedStrings(rows, arrText, endTable);
+        fixNumbering(rows);
     }
 
-    //Вернет последнюю строку в стаблице
-    private int getLowestBorerTable(String[] text) {
-        int endTable = text.length - 3;
-        for (int i = 0; i < text.length; i++) {
-            if (text[i].equals("Выписка из ЕГРЮЛ")) {
+
+    /**
+     * Вернет последнюю строку в таблице выписки.
+     *
+     * @param textArr массив из всего текста PDF-выписки, но разделенный функцией split по переносу строки.
+     */
+    private int getLowestBorerTable(String[] textArr) {
+        int endTable = textArr.length - 3;
+        for (int i = 0; i < textArr.length; i++) {
+            if (textArr[i].equals("Выписка из ЕГРЮЛ")) {
                 endTable = i - 1;
                 break;
             }
@@ -81,18 +215,26 @@ public class Converter {
         return endTable;
     }
 
-    //Вставляет нумерованные строчки из таблицы выписки в очередь
-    // Если очередь не пустая, при обработке более одной страницы, то вставляет строчки из большей страницы вперед
-    //Например, для одной страницы (очередь пуста) -> 1,2,3,4,5,6,7,8,9
-    //Например, для двух страниц -> 10,11,12,13,14,15,1,2,3,4,5,6,7,8,9
-    private void identifyNumberedLines(ArrayDeque<String> rows, String[] text) {
+
+    /**
+     * Вставляет нумерованные строчки из таблицы выписки в очередь.
+     * Если очередь не пустая, при обработке более одной страницы, то вставляет строчки из большей страницы вперед.
+     * <div><pre>
+     * Например, для одной страницы (очередь пуста) -&gt; 1,2,3,4,5,6,7,8,9
+     * Например, для двух страниц -&gt; 10,11,12,13,14,15,1,2,3,4,5,6,7,8,9
+     * </pre></div>
+     *
+     * @param rows    очередь @{@link ArrayDeque}, в которую загружаются строки таблицы.
+     * @param textArr массив из всего текста PDF-выписки, но разделенный функцией split по переносу строки.
+     */
+    private void identifyNumberedLines(ArrayDeque<String> rows, String[] textArr) {
         int sizeOld = rows.size();
         boolean isNotEmpty = sizeOld > 0;
         Pattern pattern = Pattern.compile("^\\d{1,4}\\s\\D{1,}");
 
-        for (int i = 0; i < text.length; i++) {
-            Matcher matcher = pattern.matcher(text[i]);
-            if (matcher.find()) rows.add(text[i]);
+        for (int i = 0; i < textArr.length; i++) {
+            Matcher matcher = pattern.matcher(textArr[i]);
+            if (matcher.find()) rows.add(textArr[i]);
         }
 
         if (isNotEmpty) {
@@ -102,25 +244,36 @@ public class Converter {
         }
     }
 
-    //Соединяет все строки, которое находятся внутри одной нумерованной строки таблицы, но имеют перенос
-    //Например, без ассоциации, ниже
-    //1 Полное наименование на русском языке ОБЩЕСТВО С ОГРАНИЧЕННОЙ (в PDF одна строка)
-    //ОТВЕТСТВЕННОСТЬЮ "Дельта"
-    //2 ГРН и дата внесения в ЕГРЮЛ записи,
-    //содержащей указанные сведения
-    //1000000000000
-    //11.12.2002
-    //С ассоциацией превратиться в ->
-    //1 Полное наименование на русском языке ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "Дельта"
-    //2 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения 1000000000000 11.12.2002
-    private void concatAssociatedStrings(ArrayDeque<String> rows, String[] text, int endTable) {
+
+    /**
+     * Соединяет все строки, которое находятся внутри одной нумерованной ячейки таблицы, но имеют перенос.
+     * Например, без ассоциации, ниже
+     * <div><pre>
+     * 1 Полное наименование на русском языке ОБЩЕСТВО С ОГРАНИЧЕННОЙ
+     * ОТВЕТСТВЕННОСТЬЮ "Дельта"
+     * 2 ГРН и дата внесения в ЕГРЮЛ записи,
+     * содержащей указанные сведения
+     * 1000000000000
+     * 11.12.2002
+     * </pre></div>
+     * С ассоциацией превратиться в
+     * <div><pre>
+     * 1 Полное наименование на русском языке ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "Дельта"
+     * 2 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения 1000000000000 11.12.2002
+     * </pre></div>
+     *
+     * @param rows     очередь @{@link ArrayDeque}, в которую загружаются строки таблицы.
+     * @param textArr  массив из всего текста PDF-выписки, но разделенный функцией split по переносу строки.
+     * @param endTable - индекс последней строки из textArr, где заканчивается таблица на странице.
+     */
+    private void concatAssociatedStrings(ArrayDeque<String> rows, String[] textArr, int endTable) {
         for (int i = 0; i < endTable; i++) {
-            String str = text[i];
+            String str = textArr[i];
             if (str.equals(rows.getFirst())) {
                 int j = 1;
                 String s = rows.poll();
-                while (j < endTable - i && !text[i + j].equals(rows.getFirst())) {
-                    s = s + " " + text[i + j];
+                while (j < endTable - i && !textArr[i + j].equals(rows.getFirst())) {
+                    s = s + " " + textArr[i + j];
                     j++;
                 }
                 rows.add(s);
@@ -128,15 +281,24 @@ public class Converter {
         }
     }
 
-    //Убирает строки, которые нарушают нумерацию строчек.
-    //Обычно это даты, которые распознались как номер строки, например, 1 июля - будет распознано, как отдельная строка таблицы
-    // 15  Наименование органа, зарегистрировавшего юридическое лицо до ...
-    // 1 июля 2002 года ...
-    // 16 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения ...
-    //Превратит в
-    // 15  Наименование органа, зарегистрировавшего юридическое лицо до 1 июля 2002 года ...
-    // 16 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения ...
-    //Данный метод соединяет предыдущую строку с ошибочной строкой.
+
+    /**
+     * Убирает строки, которые нарушают нумерацию строчек.
+     * Обычно это даты, которые были распознаны как номер строки, например, 1 июля - будет распознано, как отдельная строка таблицы. Данный метод соединяет предыдущую строку с ошибочной строкой.
+     * Например,
+     * <div><pre>
+     * 15 Наименование органа, зарегистрировавшего юридическое лицо до ...
+     * 1 июля 2002 года ...
+     * 16 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения ...
+     * </pre></div>
+     * Превратит в
+     * <div><pre>
+     * 15 Наименование органа, зарегистрировавшего юридическое лицо до 1 июля 2002 года ...
+     * 16 ГРН и дата внесения в ЕГРЮЛ записи, содержащей указанные сведения ...
+     * </pre></div>
+     *
+     * @param rows очередь @{@link ArrayDeque}, в которую загружаются строки таблицы.
+     */
     private void fixNumbering(ArrayDeque<String> rows) {
         Pattern pattern = Pattern.compile("^\\d{1,4}\\s");
         int lastNum = 0;
@@ -154,6 +316,20 @@ public class Converter {
                     rows.add(str);
                 }
             }
+        }
+    }
+
+    /**
+     * Считывает из файла templates.csv шаблоны поиска, которые должны соответствовать значениям ячеек во втором столбце PDF-выписки.
+     */
+    private void setTemplates() throws URISyntaxException, IOException {
+        templates = new ArrayList<String>();
+        URL res = getClass().getClassLoader().getResource("templates.csv");
+        File fileTemplates = Paths.get(res.toURI()).toFile();
+        BufferedReader br = new BufferedReader(new FileReader(fileTemplates, StandardCharsets.UTF_8));
+        String s;
+        while ((s = br.readLine()) != null) {
+            templates.add(s);
         }
     }
 
@@ -295,4 +471,13 @@ public class Converter {
     public ArrayDeque<String> getRows() {
         return rows;
     }
+
+    public ArrayList<String> getTemplates() {
+        return templates;
+    }
+
+    public void setTemplates(ArrayList<String> templates) {
+        this.templates = templates;
+    }
+
 }
--- src/main/java/org/example/ConverterBoot.java
+++ src/main/java/org/example/ConverterBoot.java
@@ -8,12 +8,22 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 public class ConverterBoot {
     public static void main(String[] args) {
+        //TODO: в будущем путь до файла надо брать из args
         try {
             Converter converter = new Converter("C:/Users/Programmer/res/vipiska.pdf");
-            converter.splitTextToRowsTable(1,1);
+            ArrayDeque<String> rows = converter.splitTextToRowsTable(1,3);
+            ArrayList<String> templates = converter.getTemplates();
+//            for(String s : templates) System.out.println(s);
+//            for (String s : rows) System.out.println(s);
+            converter.findJsonKeys();
+           //System.out.println(converter.makeRegularExp("ОГРН%1%2;ОГРН; \\s[0-9]{12};[0-9]{1}".split(";"))[1]);
+
 //            System.out.println(text);
 //            float h = converter.findUpperBorderText(1);
 //            System.out.println("Border Y="+h);
@@ -21,7 +31,7 @@ public class ConverterBoot {
             //ArrayList<String> lines = converter.extractText(1);
 //            converter.extractText();
 //            for(String s : converter.allLines) System.out.print(s);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
